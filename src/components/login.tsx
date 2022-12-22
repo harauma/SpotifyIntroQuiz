87d5b764f50dfe78a1cfff17283c27dd4e1ecbc0
原　涼馬 <fot39hara@gmail.com>
2022-12-22 21:31:08 +0900

    next/Link codemod実行

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -5,8 +5,8 @@ export const Login: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Link href="/api/auth/login">
-          <a className="btn-spotify">Login with Spotify</a>
+        <Link href="/api/auth/login" className="btn-spotify">
+          Login with Spotify
         </Link>
       </header>
     </div>
